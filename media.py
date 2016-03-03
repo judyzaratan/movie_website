692d970400711a62dc6f5c0d194533ed6555c6dc
mochicat8 <mochicat8@gmail.com>
2016-03-02 23:59:17 -0800

    Updates formatting

--- media.py
+++ media.py
@@ -1,11 +1,16 @@
 import webbrowser
+
+
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-    #underscore to tell user that is reserved
-    #creates space in memory for that instance
+    # underscore to tell user that is reserved
+    # creates space in memory for that instance
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
+    # open trailer video popup
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
